a09cc882d2b1663c37582c1ea1a546b2c3be0bea
Indiana Rhodes <rhodesindiana@gmail.com>
2019-04-04 12:22:37 -0400

    Changed input from 2 parameters to a single list of args

--- Integrate_Function.py
+++ Integrate_Function.py
@@ -4,7 +4,12 @@ import math
 def integrand(x):
     return x*x
 
-def integrate(x_min, x_max, n_boxes):
+def integrate(x_range_list):
+    
+    x_min = x_range_list[0]  
+    x_max = x_range_list[1]
+    n_boxes = x_range_list[2]
+    
     delta_x = (x_max - x_min)/n_boxes
     
     sum = (-1/2)*delta_x*(integrand(x_min) + integrand(x_max))
